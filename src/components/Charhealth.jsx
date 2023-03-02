640aaddfa806362c0813f8627bd76b774466125f
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2023-03-02 14:59:40 -0300

    Turned Charhealth and CharResistances into a separate component, Cleaned code, reduced size of charsheet

---
+++ src/components/Charhealth.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { CustomInput } from "./Inputs";
+import { healthInitialState } from "../utils/healthInitialState";
+
+
+function Charhealth({ vitalStats = { healthInitialState }, setVitalStats }) {
+  vitalStats.hpCurrent = +vitalStats.hpMax - (+vitalStats.wounds + +vitalStats.woundsNonLethal);
+  
+  return (
+    <div className="charHealth">
+      <CustomInput
+        type="number"
+        id="charHealthMax"
+        name="hpMax"
+        label="HP"
+        min={1}
+        value={vitalStats.hpMax}
+        onChange={({ target: { value } }) =>
+          setVitalStats({ ...vitalStats, hpMax: value })
+        }
+      />
+      <CustomInput
+        type="number"
+        id="charHealthCurrent"
+        name="hpCurrent"
+        label="Current HP"
+        value={vitalStats.hpCurrent}
+        readOnly
+      />
+      <CustomInput
+        type="number"
+        id="charWounds"
+        name="hpWounds"
+        label="Wounds"
+        min={0}
+        value={vitalStats.wounds}
+        onChange={({ target: { value } }) => {
+           setVitalStats({ ...vitalStats, wounds: value});
+        }
+        }
+      />
+      <CustomInput
+        type="number"
+        id="charWoundsNonLethal"
+        name="hpWoundsNonLethal"
+        label="NonLethal Wounds"
+        min={0}
+        value={vitalStats.woundsNonLethal}
+        onChange={({ target: { value } }) =>
+          setVitalStats({ ...vitalStats, woundsNonLethal: value})
+        }
+      />
+    </div>
+  );
+}
+
+export default Charhealth;
--- src/components/Charsheet.jsx
+++ src/components/Charsheet.jsx
@@ -3,10 +3,11 @@ import { charHeaderInitialState as charInfo } from "../utils/charHeaderInitialSt
 import { charAbilityScoreInitialState as scores } from "../utils/charAbilityScoreInitialState";
 import { healthInitialState as vitals } from "../utils/healthInitialState";
 import { alignments } from "../utils/alignment";
-import { drBypassTypes } from "../utils/drBypassTypes";
 import Ability from "./Ability";
 
 import { CustomInput, SelectCustomInput } from "./Inputs";
+import Charhealth from "./Charhealth";
+import CharResistances from "./CharResistances";
 
 function Charsheet() {
   const [charHeader, setCharHeader] = useState(charInfo);
@@ -183,8 +184,8 @@ function Charsheet() {
           }
         />{" "}
       </header>
-      <main className="charMainStats">
-        <section>
+      <main>
+        <section className="charMainStats">
           <div className="charAbilities">
             <div>
               {Object.keys(abilityScores).map((score) => (
@@ -198,51 +199,8 @@ function Charsheet() {
               ))}
             </div>
           </div>
-          <div className="charHealth">
-            <CustomInput
-              type="number"
-              id="charHealthMax"
-              name="hpMax"
-              label="HP"
-              min={1}
-              value={vitalStats.hpMax}
-              onChange={({ target: { value } }) =>
-                setVitalStats({ ...vitalStats, hpMax: value })
-              }
-            />
-            <CustomInput
-              type="number"
-              id="charHealthCurrent"
-              name="hpCurrent"
-              label="Current HP"
-              readOnly
-              value={
-                +vitalStats.hpMax -
-                (+vitalStats.wounds + +vitalStats.woundsNonLethal)
-              }
-            />
-            <CustomInput
-              type="number"
-              id="charWounds"
-              name="hpWounds"
-              label="Wounds"
-              min={0}
-              value={vitalStats.wounds}
-              onChange={({ target: { value } }) =>
-                setVitalStats({ ...vitalStats, wounds: value })
-              }
-            />
-            <CustomInput
-              type="number"
-              id="charWoundsNonLethal"
-              name="hpWoundsNonLethal"
-              label="NonLethal Wounds"
-              min={0}
-              value={vitalStats.woundsNonLethal}
-              onChange={({ target: { value } }) =>
-                setVitalStats({ ...vitalStats, woundsNonLethal: value })
-              }
-            />
+          <div className="charHealthContainer">
+            <Charhealth vitalStats={vitalStats} setVitalStats={setVitalStats} />
           </div>
           <div className="charInitiative">
             <CustomInput
@@ -254,40 +212,8 @@ function Charsheet() {
               value={Math.floor((+abilityScores.dex - 10) / 2)}
             />
           </div>
-          <div className="charResistances">
-            <CustomInput
-              type="number"
-              id="charDR"
-              name="damageReduction"
-              label="DR"
-              min={0}
-              value={vitalStats.DR}
-              onChange={({ target: { value } }) =>
-                setVitalStats({ ...vitalStats, DR: value })
-              }
-            />
-            <SelectCustomInput
-              id="charDRBypassType"
-              label="Bypass: "
-              value={vitalStats.drBypass}
-              onChange={({ target: { value } }) =>
-                setVitalStats({ ...vitalStats, drBypass: value })
-              }
-              options={ drBypassTypes.map((type) => (
-                <option key={type} value={type}>{type}</option>
-              )) }
-            />
-            <CustomInput
-              type="number"
-              id="charSR"
-              name="spellResistance"
-              label="SR"
-              min={0}
-              value={vitalStats.SR}
-              onChange={({ target: { value } }) =>
-                setVitalStats({ ...vitalStats, SR: value })
-              }
-            />
+          <div className="charResistancesContainer">
+            <CharResistances vitalStats={vitalStats} setVitalStats={setVitalStats} />
           </div>
         </section>
         <section>Skill Section</section>
