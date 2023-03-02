80a65bb9802e37f45ec3ee6c5426457d1fca15e2
Lucas Muniz Lara <lucasml@live.com>
2023-03-01 22:28:45 -0300

    css vanilla global

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { Switch, Route } from 'react-router-dom';
-import PlaceHolder from './pages/PlaceHolder';
+import { Switch, Route } from "react-router-dom";
+import PlaceHolder from "./pages/PlaceHolder";
+import "./App.css";
 
 function App() {
   return (
     <Switch>
-      <Route exact component={ PlaceHolder } path="/" />
+      <Route exact component={PlaceHolder} path="/" />
     </Switch>
   );
 }
