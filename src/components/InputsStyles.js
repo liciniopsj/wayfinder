792f20693cf412a454567a4f5927a0d2cec66508
Lucas Muniz Lara <lucasml@live.com>
2023-03-02 00:33:22 -0300

    Input Styles

---
+++ src/components/InputsStyles.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+const InputDefault = styled.input`
+  font-size: 18px;
+  padding: 10px;
+  margin: 10px;
+  background: papayawhip;
+  border: none;
+  border-radius: 3px;
+  ::placeholder {
+    color: palevioletred;
+  }
+`;
+
+const SelectInput = styled.select`
+  width: 100%;
+  height: 35px;
+  background: white;
+  color: gray;
+  padding-left: 5px;
+  font-size: 14px;
+  border: none;
+  margin-left: 10px;
+
+  option {
+    color: black;
+    background: white;
+    display: flex;
+    white-space: pre;
+    min-height: 20px;
+    padding: 0px 2px 1px;
+  }
+`;
+
+export { InputDefault, SelectInput };
